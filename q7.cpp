695c8b23fc582fdc511f4fb1712a4c9853156097
vishal-1712 <vishalrawata10@gmail.com>
2026-01-26 20:52:38 +0530

    comparing to string

--- q7.cpp
+++ q7.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main(){
   int arr[5][5];
   int i,j,c,r;
-  for(i=1;i<=5;i++){
-   for(j=1;j<=i;j++){
+  for(i=0;i<5;i++){
+   for(j=0;j<5;j++){
     cin>>arr[i][j];
 if(arr[i][j]==1){
-  r=abs(i-3);
-  c=abs(j-3);
+  r=abs(i-2);
+  c=abs(j-2);
    }
  }
- cout<<r+c<<endl;
-  }
-  
+}
+   cout<<r+c<<endl;
+
   return 0;
 }
